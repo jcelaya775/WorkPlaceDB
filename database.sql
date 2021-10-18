4fac902f97448d4740dd5ab5ff78ef3872fac801
Jorge Celaya <jcelaya775@gmail.com>
2021-10-17 21:45:04 -0500

    added more data

--- database.sql
+++ database.sql
@@ -6,11 +6,17 @@ CREATE TABLE employee (
 );
   
 INSERT INTO employee VALUES
-    ("Steve", "Los Angeles", 45000),
-    ("Jonathan", "Chicago", 50000),
+    ("Steve", "Chicago", 45000),
+    ("Jonathan", "Newark", 50000),
     ("Angela", "Brooklyn", 65000),
-    ("Nick", "Naperville", 35000),
-    ("Mark", "New Jersey", 55000);
+    ("Nick", "Jersey City", 85000),
+    ("Mark", "Jersey City", 55000),
+    ("Pat", "New York City", 60000),
+    ("Rochelle", "Jersey City", 55000),
+    ("Jamie", "Manhattan", 60000),
+    ("Dennis", "San Jose", 50000),
+    ("Brianna", "Boston", 85000),
+    ("Robert", "Sacramento", 40000);
 
 CREATE TABLE works (
     empname VARCHAR(10),
@@ -21,12 +27,19 @@ CREATE TABLE works (
 );
 
 INSERT INTO works VALUES
-    ("Angela", "development"),
-    ("Jonathan", "business analytics"),
-    ("Nick", "marketing"),
-    ("Steve", "marketing"),
-    ("Mark", "d
-    evelopment");
+    ("Steve", "Marketing"),
+    ("Jonathan", "Analytics"),
+    ("Jonathan", "Development"),
+    ("Angela", "Development"),
+    ("Nick", "Analytics"),
+    ("Mark", "Network Security"),
+    ("Pat", "Analytics"),
+    ("Rochelle", "Development"),
+    ("Jamie", "Development"),
+    ("Dennis", "Communications"),
+    ("Brianna", "Network Security"),
+    ("Brianna", "Development"),
+    ("Robert", "Communications");
 
 CREATE TABLE unit (
     unitname VARCHAR(25),
@@ -36,23 +49,28 @@ CREATE TABLE unit (
 
 INSERT INTO unit VALUES 
     ("Development", "Brooklyn"),
-    ("Business Analytics", "Chicago"),
-    ("Marketing", "Chicago");
+    ("Analytics", "Manhattan"),
+    ("Marketing", "Chicago"),
+    ("Communications", "San Francisco"),
+    ("Network Security", "Boston");
 
 CREATE TABLE manages (
     empname VARCHAR(25),
     mgrname VARCHAR(25),
     PRIMARY KEY (empname, mgrname)
     FOREIGN KEY (empname) REFERENCES employee(empname)
-    FOREIGN KEY (mgrname) REFERENCES employee(mgrname)
+    FOREIGN KEY (mgrname) REFERENCES employee(empname)
 );
 
 INSERT INTO manages VALUES 
-    ("Angela", "Boe"),
-    ("Jonathan", "Ed"),
-    ("Nick", "Anna"),
-    ("Steve", "Marissa"),
-    ("Mark", "Kevin");
+    ("Jonathan", "Nick"), -- analytics
+    ("Pat", "Nick"), -- analytics
+    ("Jonathan", "Brianna"), -- development
+    ("Angela", "Brianna"), -- development
+    ("Rochelle", "Brianna"), -- development
+    ("Jamie", "Brianna"), -- development
+    ("Robert", "Dennis"), -- communications/marketing
+    ("Steve", "Dennis"); -- communications/marketing
 
 -- Prolem 1a)
 SELECT e.empname, e.salary, w.unitname
